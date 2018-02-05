bc84c54f7450edd7d00c2e80a497bd9aae1045c7
Yegor Bugayenko <yegor256@gmail.com>
2018-02-05 09:22:53 +0300

    #13 deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ set -e
 cd $(dirname $0)
 bundle update
 rake
+trap 'git reset HEAD~1 && rm config.yml && git checkout -- .gitignore' EXIT
 sed -i -s 's|Gemfile.lock||g' .gitignore
 git add Gemfile.lock
 git add .gitignore
 git commit -m 'configs for heroku'
-trap 'git reset HEAD~1 && rm config.yml && git checkout -- .gitignore' EXIT
 git push heroku master -f
 
