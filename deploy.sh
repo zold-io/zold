ebdee0602522842e17e75297356b970c847bcfe3
Yegor Bugayenko <yegor256@gmail.com>
2018-06-10 09:54:17 +0300

    #143 bonus key deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ set -e
 cd $(dirname $0)
 bundle update
 rake
-trap 'git reset HEAD~1 && git checkout -- .gitignore' EXIT
+trap 'git reset HEAD~1 && rm bonus.key && git checkout -- .gitignore' EXIT
+cp /code/home/assets/zold/bonus.key .
 sed -i -s 's|Gemfile.lock||g' .gitignore
+git add bonus.key
 git add Gemfile.lock
 git add .gitignore
 git commit -m 'configs for heroku'
