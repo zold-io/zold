3c30059140c12c78e83c0c0bffb095fb8432a01d
James C Russell <james@101ideas.cz>
2018-06-30 17:33:07 +0200

    Fix the pdd comments #285
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Sat Jun 30 17:33:07 2018 +0200
    #
    # On branch 285
    # Your branch is up to date with 'origin/285'.
    #
    # Changes to be committed:
    #	modified:   lib/zold/version_upgrade_manager.rb
    #
    # Changes not staged for commit:
    #	modified:   test/test__helper.rb
    #

--- lib/zold/version_upgrade_manager.rb
+++ lib/zold/version_upgrade_manager.rb
@@ -3,15 +3,15 @@ require 'semantic'
 module Zold
   # Class to manage data upgrades (when zold itself upgrades).
   class VersionUpgradeManager
-    # @todo #285 Refactor so we don't just assume there's .zoldata in the current directory.
+    # @todo #285:30min Refactor so we don't just assume there's .zoldata in the current directory. Possibly something like a config object should be introduced.
     #
-    # @todo #285 Communicate the ongoing upgrade and its steps to the user.
+    # @todo #285:30min Communicate the ongoing upgrade and its steps to the user.
     #  The user should know:
     #  - If the upgrade is happening.
     #  - What upgrade scripts are run.
     #  - On failure there should be a report of what failed.
     #
-    # @todo #285 Write the upgrade manager tests that ensure:
+    # @todo #285:30min Write the upgrade manager tests that ensure:
     #  - Nothing breaks without the version file.
     #  - The upgrade scripts run when there is a version file and there are pending upgrade scripts.
     #  - Make sure *only* the correct upgrade scripts run.
@@ -29,8 +29,9 @@ module Zold
       version_string = File.read('.zoldata/version')
       Semantic::Version.new(version_string)
     rescue Errno::ENOENT
-      # @todo #285 Save version if there is no .zoldata/version yet
-      #  This is breaking the specs, it needs additional time.
+      # @todo #285:30min Save the version if there is no .zoldata/version
+      #  present yet. This is breaking the specs, it needs some additional time.
+
       # File.open('.zoldata/version', 'w') do |file|
       #   file.puts(Zold::VERSION)
       # end
@@ -38,7 +39,7 @@ module Zold
       false
     end
 
-    # @todo #285 Compare from_version with the current version and run all the scripts
+    # @todo #285:30min Compare from_version with the current version and run all the scripts
     #  from the upgrades directory that are in between these two versions.
     def self.upgrade(from_version); end
   end
