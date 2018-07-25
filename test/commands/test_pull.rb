d42a6d44fb93698cc8b9850e8ef95149578dbdc1
Emiliano Della Casa <emiliano.dellacasa@gmail.com>
2018-07-25 17:28:04 +0200

    Issue #324 - Adding new test for pull showing how to mock a pull wallet

---
+++ test/commands/test_pull.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2018 Yegor Bugayenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'minitest/autorun'
+require 'webmock/minitest'
+require_relative '../fake_home'
+require_relative '../test__helper'
+require_relative '../../lib/zold/id'
+require_relative '../../lib/zold/json_page'
+require_relative '../../lib/zold/commands/pull'
+
+# PUSH test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2018 Yegor Bugayenko
+# License:: MIT
+class TestPull < Minitest::Test
+  def test_pull_wallet
+    FakeHome.new.run do |home|
+      remotes = home.remotes
+      remotes.add('localhost', 80)
+      wallet_json = home.create_wallet_json
+
+      wallet_id = Zold::JsonPage.new(wallet_json).to_hash['id']
+      stub_request(:get, "http://localhost:80/wallet/#{wallet_id}").to_return(status: 200, body: wallet_json)
+      Zold::Pull.new(wallets: home.wallets, remotes: remotes, copies: home.copies.root.to_s, log: test_log).run(
+        ['--ignore-this-stupid-option', 'pull', wallet_id.to_s]
+      )
+      assert(home.wallets.find(Zold::Id.new(wallet_id)).exists?)
+    end
+  end
+end
