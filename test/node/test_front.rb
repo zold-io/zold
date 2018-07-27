600d3a9e1714d675be34b3524d49c754a4e6339a
theterminalguy <damiansimonpeter@gmail.com>
2018-07-27 03:35:40 +0100

    Refactor to avoid testing private methods
    - Resolve to using stub HTTP web interface

--- test/node/test_front.rb
+++ test/node/test_front.rb
@@ -184,32 +184,41 @@ class FrontTest < Minitest::Test
   end
 
   def test_headers_are_being_set_correctly
-    FakeNode.new(log: test_log).run(['--ignore-score-weakness']) do |port|
-      response = Zold::Http.new(uri: URI("http://localhost:#{port}/"), score: nil).get
-      assert_equal(
-        'no-cache',
-        response.header['Cache-Control']
-      )
-      assert_equal(
-        'close',
-        response.header['Connection']
-      )
-      assert_equal(
-        app.settings.version,
-        response.header['X-Zold-Version']
-      )
-      assert_equal(
-        app.settings.protocol.to_s,
-        response.header[Zold::Http::PROTOCOL_HEADER]
-      )
-      assert_equal(
-        '*',
-        response.header['Access-Control-Allow-Origin']
-      )
-      assert_equal(
-        app.new!.http_score_header.strip,
-        response.header[Zold::Http::SCORE_HEADER]
-      )
-    end
+    headers = {
+      'Cache-Control': 'no-cache',
+      'Connection': 'close',
+      'X-Zold-Version': app.settings.version,
+      'Access-Control-Allow-Origin': '*'
+    }
+    headers[Zold::Http::PROTOCOL_HEADER] = app.settings.protocol.to_s
+    headers[Zold::Http::SCORE_HEADER] = Zold::Score::ZERO.to_s
+    stub_request(:get, 'http://localhost:888/').to_return(
+      headers: headers
+    )
+    response = Zold::Http.new(uri: 'http://localhost:888/', score: nil).get
+    assert_equal(
+      'no-cache',
+      response.header['Cache-Control']
+    )
+    assert_equal(
+      'close',
+      response.header['Connection']
+    )
+    assert_equal(
+      app.settings.version,
+      response.header['X-Zold-Version']
+    )
+    assert_equal(
+      app.settings.protocol.to_s,
+      response.header[Zold::Http::PROTOCOL_HEADER]
+    )
+    assert_equal(
+      '*',
+      response.header['Access-Control-Allow-Origin']
+    )
+    assert_equal(
+      Zold::Score::ZERO.to_s,
+      response.header[Zold::Http::SCORE_HEADER]
+    )
   end
 end
