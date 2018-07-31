f23493919f75688ab7609fd1885d7c5261c67397
Yegor Bugayenko <yegor256@gmail.com>
2018-08-01 01:05:19 +0300

    #436 typo

--- fixtures/scripts/distribute-wallet.sh
+++ fixtures/scripts/distribute-wallet.sh
@@ -75,7 +75,7 @@ fi
 # Now, we remove the wallet from the second node and expect the first
 # one to "spread" it again, almost immediately.
 rm ${second}/0000000000000000.z
-  (( i = 0 ))
+(( i = 0 ))
 until zold fetch 0000000000000000 --ignore-score-weakness; do
   echo 'Failed to fetch, let us try again'
   (( i++ ))
