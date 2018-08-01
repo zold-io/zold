f479cec4d5a194c9a2f098e668a7ae81233d3eb1
Yegor Bugayenko <yegor256@gmail.com>
2018-08-01 06:23:54 +0300

    #436 formatting

--- fixtures/scripts/_head.sh
+++ fixtures/scripts/_head.sh
@@ -12,11 +12,11 @@ function reserve_port {
 }
 
 function wait_for_url {
-  ((p = 0))
+  ((i=0))
   while ! curl --silent --fail $1 > /dev/null; do
-    ((p++))
-    if ((p==30)); then
-      echo "URL $1 is not available after $p seconds of waiting"
+    ((i++))
+    if ((i==30)); then
+      echo "URL $1 is not available after ${i} attempts"
       exit 12
     fi
     sleep 5
@@ -24,11 +24,11 @@ function wait_for_url {
 }
 
 function wait_for_port {
-  ((p = 0))
+  ((i=0))
   while ! nc -z localhost $1; do
-    ((p++))
-    if ((p==30)); then
-      echo "Port $1 is not available after $p seconds of waiting"
+    ((i++))
+    if ((i==30)); then
+      echo "Port $1 is not available after ${i} attempts"
       exit 13
     fi
     sleep 5
@@ -36,11 +36,11 @@ function wait_for_port {
 }
 
 function wait_for_file {
-  ((c = 0))
+  ((i=))
   while [ ! -f $1 ]; do
-    ((c++))
-    if ((c==30)); then
-      echo "File $1 not found, giving up after $c seconds of waiting"
+    ((i++))
+    if ((i==30)); then
+      echo "File $1 not found, giving up after ${i} attempts"
       exit 14
     fi
     sleep 5
@@ -51,14 +51,14 @@ function halt_nodes {
   for p in "$@"; do
     pid=$(curl --silent "http://localhost:$p/pid?halt=test" || echo 'absent')
     if [[ "${pid}" =~ ^[0-9]+$ ]]; then
-      ((c = 0))
+      ((i=0))
       while kill -0 ${pid}; do
-        ((c++))
-        if ((c==30)); then
-          echo "Waiting for process ${pid} to die"
+        ((i++))
+        if ((i==30)); then
+          echo "Process ${pid} didn't die, it's a bug"
           exit 15
         fi
-        echo "Still waiting for process ${pid} to die, cycle no.${c}"
+        echo "Still waiting for process ${pid} to die, attempt no.${i}"
         sleep 5
       done
       echo "Process ${pid} is dead!"
