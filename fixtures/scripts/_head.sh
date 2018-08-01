443f33ddc077994b1908878af9d2ba4929e1afbb
Yegor Bugayenko <yegor256@gmail.com>
2018-08-01 06:21:08 +0300

    #436 formatting

--- fixtures/scripts/_head.sh
+++ fixtures/scripts/_head.sh
@@ -12,10 +12,10 @@ function reserve_port {
 }
 
 function wait_for_url {
-  (( p = 0 ))
+  ((p = 0))
   while ! curl --silent --fail $1 > /dev/null; do
-    (( p++ ))
-    if (( p==30 )); then
+    ((p++))
+    if ((p==30)); then
       echo "URL $1 is not available after $p seconds of waiting"
       exit 12
     fi
@@ -24,10 +24,10 @@ function wait_for_url {
 }
 
 function wait_for_port {
-  (( p = 0 ))
+  ((p = 0))
   while ! nc -z localhost $1; do
-    (( p++ ))
-    if (( p==30 )); then
+    ((p++))
+    if ((p==30)); then
       echo "Port $1 is not available after $p seconds of waiting"
       exit 13
     fi
@@ -36,10 +36,10 @@ function wait_for_port {
 }
 
 function wait_for_file {
-  (( c = 0 ))
+  ((c = 0))
   while [ ! -f $1 ]; do
-    (( c++ ))
-    if (( c==30 )); then
+    ((c++))
+    if ((c==30)); then
       echo "File $1 not found, giving up after $c seconds of waiting"
       exit 14
     fi
@@ -51,10 +51,10 @@ function halt_nodes {
   for p in "$@"; do
     pid=$(curl --silent "http://localhost:$p/pid?halt=test" || echo 'absent')
     if [[ "${pid}" =~ ^[0-9]+$ ]]; then
-      (( c = 0 ))
+      ((c = 0))
       while kill -0 ${pid}; do
-        (( c++ ))
-        if (( c==30 )); then
+        ((c++))
+        if ((c==30)); then
           echo "Waiting for process ${pid} to die"
           exit 15
         fi
--- fixtures/scripts/distribute-wallet.sh
+++ fixtures/scripts/distribute-wallet.sh
@@ -44,11 +44,11 @@ zold remote add localhost ${second}
 # to be visible there. We are doing a number of attempts with a small
 # delay between them, in order to give the first node a chance to distribute
 # the wallet.
-(( i = 0 ))
+((i = 0))
 until zold fetch 0000000000000000 --ignore-score-weakness; do
   echo 'Failed to fetch, let us try again'
-  (( i++ ))
-  if (( i==5 )); then
+  ((i++))
+  if ((i==5)); then
     cat ${first}/log.txt
     echo "The wallet has not been distributed, after ${i} attempts"
     exit 9
@@ -75,11 +75,11 @@ fi
 # Now, we remove the wallet from the second node and expect the first
 # one to "spread" it again, almost immediately.
 rm ${second}/0000000000000000.z
-(( i = 0 ))
+((i = 0))
 until zold fetch 0000000000000000 --ignore-score-weakness; do
   echo 'Failed to fetch, let us try again'
-  (( i++ ))
-  if (( i==5 )); then
+  ((i++))
+  if ((i==5)); then
     cat ${first}/log.txt
     echo "The wallet 0000000000000000 has not been spread, after ${i} attempts"
     exit 8
