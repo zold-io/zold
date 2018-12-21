9e6ad3f42d00a01c562f8dc3a69e468dc062d8bd
Yegor Bugayenko <yegor256@gmail.com>
2018-12-21 11:16:59 +0300

    #oom: #646 GC removed

--- lib/zold/hungry_wallets.rb
+++ lib/zold/hungry_wallets.rb
@@ -74,7 +74,6 @@ module Zold
         sleep 1
         return
       end
-      GC.start
       begin
         Pull.new(wallets: @wallets, remotes: @remotes, copies: @copies, log: @log).run(
           ['pull', id.to_s, "--network=#{@network}", '--tolerate-edges', '--tolerate-quorum=1']
