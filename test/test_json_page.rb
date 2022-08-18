e47abb92d4f6d1d4fa2729eb5521dd892f27b9cb
davvd <davvd@zold.io>
2022-08-18 05:26:26 +0300

    #797 year up

--- test/test_json_page.rb
+++ test/test_json_page.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2018-2020 Zerocracy, Inc.
+# Copyright (c) 2018-2022 Zerocracy, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
